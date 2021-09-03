eb505f0a7aceac5f61164c371d864ce73ceaebe9
Mitchell <Hmitchell.hall@gmail.com>
2021-09-03 08:17:18 -0500

    working fetch with test data

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 console.log('hello')
+
+function postItem(){
+    fetch("http://localhost:3000/beerInv")
+    .then((resp) => resp.json())
+    .then((beerObj) => console.log(beerObj))
+}
+postItem()
