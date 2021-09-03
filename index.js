dab1f5efd5df409f9b61aafb067318246c43b55d
Mitchell <Hmitchell.hall@gmail.com>
2021-09-03 09:36:54 -0500

    some very basic styling added to inventory items

--- index.js
+++ index.js
@@ -9,19 +9,20 @@ fetchItem()
 
 //below we are appending our added items to the page and adding event listeners
 function appendItem(value){
-    const invDiv = document.createElement('div')
-    invDiv.textContent = value.itemName
-    document.querySelector("body > h3").append(invDiv)
+    const invDiv = document.createElement('form')
+    invDiv.className = 'invDiv'
+    invDiv.textContent = "Item Name:  " + value.itemName
+    document.querySelector("body > section").append(invDiv)
 
     const quantDiv = document.createElement('div')
-    quantDiv.textContent = value.quantity
+    quantDiv.textContent = "Quantity:  " + value.quantity
     invDiv.append(quantDiv)
 
     const colorDiv = document.createElement('div')
-    colorDiv.textContent = value.color
-    quantDiv.append(colorDiv)
+    colorDiv.textContent = "Color:  " + value.color
+    invDiv.append(colorDiv)
 
     const acidDiv = document.createElement('div')
-    acidDiv.textContent = value.alphaAcid
-    colorDiv.append(acidDiv)
+    acidDiv.textContent = "Alpha Acid %:  " + value.alphaAcid
+    invDiv.append(acidDiv)
 }
