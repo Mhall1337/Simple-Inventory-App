78be1445d137288f13be393419683aae5aec4c53
Mitchell <Hmitchell.hall@gmail.com>
2021-09-03 09:08:33 -0500

    form values successfully appending to page. need to add styling

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
 console.log('hello')
 
-function postItem(){
+function fetchItem(){
     fetch("http://localhost:3000/beerInv")
     .then((resp) => resp.json())
-    .then((beerObj) => console.log(beerObj))
+    .then((beerObj) => beerObj.forEach(value => appendItem(value)))
 }
-postItem()
+fetchItem()
+
+//below we are appending our added items to the page and adding event listeners
+function appendItem(value){
+    const invDiv = document.createElement('div')
+    invDiv.textContent = value.itemName
+    document.querySelector("body > h3").append(invDiv)
+
+    const quantDiv = document.createElement('div')
+    quantDiv.textContent = value.quantity
+    invDiv.append(quantDiv)
+
+    const colorDiv = document.createElement('div')
+    colorDiv.textContent = value.color
+    quantDiv.append(colorDiv)
+
+    const acidDiv = document.createElement('div')
+    acidDiv.textContent = value.alphaAcid
+    colorDiv.append(acidDiv)
+}
