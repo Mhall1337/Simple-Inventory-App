2d2cebeccd7b8bfc570fb4061d7bfa30e59068cf
Mitchell <Hmitchell.hall@gmail.com>
2021-09-07 12:58:25 -0500

    erased the alert because it was messing up button event and simplified button event

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
+
 console.log('hello')
+//patch an item
+function patchItem(obj){
+    fetch(`http://localhost:3000/beerInv/${obj.id}`,{
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(obj)
+  })
+  .then(res => res.json())
+  .then(beerObj => console.log(beerObj))
+  }
+
 //post an item
 function postItem(obj){
     fetch('http://localhost:3000/beerInv',{
@@ -20,21 +34,23 @@ function fetchItem(){
 }
 fetchItem()
 
+
 //below we are appending our added items to the page and adding event listeners
 function appendItem(item){
-    let inventoryDiv = document.createElement('form')
-    inventoryDiv.className = 'inventoryDiv'
-    inventoryDiv.textContent = "Item Name:  " + item.itemName
-    document.querySelector("body > section").append(inventoryDiv)
+    let inventoryForm = document.createElement('form')
+    inventoryForm.className = 'inventoryForm'
+    inventoryForm.textContent = "Item Name:  " + item.itemName
+    document.querySelector("body > section").append(inventoryForm)
 
+//<div> containing <button>, <input>, and quantity <p>
     const quantDiv = document.createElement('div')
-    //add <p> for text content to get out of string??
+
+//add <p> for text content to get out of string??
     let paragraph = document.createElement('p')
     paragraph.textContent = "Quantity:  " + item.quantity
-    console.log(item.quantity)
     quantDiv.append(paragraph)
-    inventoryDiv.append(quantDiv)
-    //createInput(quantDiv, item)
+    inventoryForm.append(quantDiv)
+    
 //add input <div>
     let quantInput = document.createElement('input')
     quantInput.id = item.id
@@ -47,35 +63,31 @@ function appendItem(item){
     quantDiv.append(quantInput)
     quantDiv.append(button)
 
-    //add button event listener
-    buttonEvent(button, item, quantInput, paragraph)
-    //create color <div>
+//create color <div>
     let colorDiv = document.createElement('div')
     colorDiv.textContent = "Color:  " + item.color
-    inventoryDiv.append(colorDiv)
-    //create Alpha Acid <div>
+    inventoryForm.append(colorDiv)
+
+//create Alpha Acid <div>
     let acidDiv = document.createElement('div')
     acidDiv.textContent = "Alpha Acid %:  " + item.alphaAcid
-    inventoryDiv.append(acidDiv)
+    inventoryForm.append(acidDiv)  
+
+//button event listener with PATCH
+    buttonEvent(button, item, quantInput, paragraph)
 }
 
-//created our input & submit that will go along with each individual inventory item
-// function createInput(element, item){
-//     const quantInput = document.createElement('input')
-//     quantInput.id = item.id
-//     quantInput.class = 'quantityInput'
-//     quantInput.placeholder = 'Amnt to Add or Remove'
-//     quantInput.type = 'text'
-//     const button = document.createElement('button')
-//     button.textContent = 'Add/Remove'
-//     button.id = item.id
-//     element.append(quantInput)
-//     element.append(button)
-//     button.addEventListener('click', e => console.log(e))
-
-//     //path[1].childNodes[1].value
+//     button.addEventListener('click', e => {
+//         e.preventDefault()
+//         console.log(item.quantity)
+//         paragraph.textContent = "Quantity:  " + (item.quantity -= (quantInput.value *-1))
+//     patchItem(item)   
+//     quantInput.value = ''
+//     }
+//     )
 // }
-// handle adding an item to inventory
+
+// handle adding an item to inventory database
 document.querySelector("#addItem > input[type=Submit]").addEventListener('click', e => addToInventory(e))
 function addToInventory(e){
     e.preventDefault()
@@ -89,17 +101,28 @@ let  newInventoryItem = {
     postItem(newInventoryItem)
 }
 
+function lowItemColorChange(item){
+   const forms = document.querySelectorAll('.inventoryForm')
+   //const lowForms = forms.filter(form => item.quantity <= 6)
+   console.log(forms.p)
+   
+//    forms.forEach(form => {
+//         if(item.quantity <= 6){
+//         form.style.background = 'red'
+//         }   
+//     })
+}
+
+
+
+// button event handler
 function buttonEvent(button, item, quantInput, paragraph){
     button.addEventListener('click', e => {
         e.preventDefault()
-        if(`${item.quantity -= (quantInput.value *-1)}` <= 0){
-            alert('NOT ENOUGH IN INVENTORY')
-        }else if(quantInput.value < 0 && `${item.quantity -= (quantInput.value *-1)}` >= 0){
-        paragraph.textContent = "Quantity:  " + `${item.quantity -= (quantInput.value *-1)}`
-        }else if(quantInput.value > 0){
         paragraph.textContent = "Quantity:  " + (item.quantity -= (quantInput.value *-1))
-        }
+        console.log(item.quantity)
     quantInput.value = ''
+    patchItem(item)
     }
     )
 }
