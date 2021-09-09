88ef2a198c23f7e008448ae512c7737abc650bba
Mitchell <Hmitchell.hall@gmail.com>
2021-09-09 12:56:01 -0500

    finished project 2.0 accidentally broke previous version and totally reworked

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # Simple-Inventory-App
-A one page app to handle simple inventory management
+A one-page app to handle simple inventory management
+
+The purpose of this app is to keep track of my brother's beer inventory after Brew Days. To add a beer to the inventory, fill out the mock form under Add an item! and click the Add button. This will add the item to the page as well as the db.json file. After it is added, you can update the inventory quantity by entering a negative number to remove from the inventory, or entering a positive number to add to the inventory. To remove a beer from the inventory, click the Remove From Inventory button. This will delete the beer from the page and the database.
+
+For this app to work, the use of JSON Server is required. 
+If you do not have JSON Server installed, execute the following command in your terminal, if you do, skip this step and start the server:
+
+npm install -g json-server
+
+After it is installed, navigate to the Simple-Inventory-App directory and start the server with the following command:
+
+json-server --watch db.json
+
+Now, all db.json information will be available and any changes made to the inventory via the app will be saved there.
+
+To exit the json server, enter ctrl + c into the terminal.
--- index.js
+++ index.js
@@ -1,16 +1,58 @@
 
+//below we are appending our added items to the page
+
+function appendItem(item){
+    let beerDiv = document.createElement('div')
+    beerDiv.className = "inventoryDiv"
+    beerDiv.id = item.id
+    beerDiv.innerHTML = `
+        <p>Beer Name: ${item.beerName}</p>
+        <div id='quantityDiv'>Quantity: ${item.quantity}</div>
+        <form id='beerForm'>
+        <input type='number' placeholder = 'Amnt to add or remove' required>
+        <input type='submit' value='Add/Remove'>
+        </form>
+        <div>Style: ${item.style}</div>
+        <div>Alocohol%: ${item.alcohol}<div>
+        <button class='delete' id='delete'> Delete Item</button>
+    `
+//Add/Remove button event function
+beerDiv.querySelector("#beerForm").addEventListener('submit', e => {
+    e.preventDefault()
+    const quantityInput = beerDiv.querySelector("#beerForm > input[type=number]")
+    beerDiv.querySelector('#quantityDiv').textContent = "Quantity:  " + (item.quantity -= (quantityInput.value *-1))
+    patchItem(item)
+    quantityInput.value = ''
+    }
+)
+
+//delete inventory item
+beerDiv.querySelector("#delete").addEventListener('click', e =>{
+    beerDiv.remove()
+    deleteItem(item.id)
+})
+//append item to page
+document.querySelector("body > section").append(beerDiv)
+}
+
+// handle adding an item to inventory database with 'click' event
+const addBeerToInventory = document.querySelector("#addItem")
+addBeerToInventory.addEventListener('submit', (e)=> addToInventory(e))
+function addToInventory(e){
+    e.preventDefault()
+let  newInventoryItem = {
+        beerName: document.querySelector("#beerName").value,
+        quantity: document.querySelector("#quantity").value,
+        style: document.querySelector("#style").value,
+        alcohol: document.querySelector("#alcohol").value
+     }
+     postItem(newInventoryItem)
+     addBeerToInventory.reset()
+}
+
+
 //patch a beer
-function patchItem(obj){
-    fetch(`http://localhost:3000/beerInv/${obj.id}`,{
-      method: 'PATCH',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(obj)
-     })
-    .then(res => res.json())
-  //.then(beerObj => console.log(beerObj))
-  }
+
 
 //post a beer
 function postItem(obj){
@@ -22,7 +64,19 @@ function postItem(obj){
       body: JSON.stringify(obj)
   })
   .then(res => res.json())
-  //.then(beerObj => console.log(beerObj))
+  .then(beerObj => appendItem(beerObj))
+  }
+
+  function patchItem(obj){
+    fetch(`http://localhost:3000/beerInv/${obj.id}`,{
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(obj)
+     })
+    .then(res => res.json())
+    .then(beerObj => console.log(beerObj))
   }
 
 //fetch a beer
@@ -42,94 +96,5 @@ function deleteItem(id){
         }
     })
     .then(res => res.json())
-    //.then(beer => console.log(beer))
-}
-
-//below we are appending our added items to the page
-function appendItem(item){
-    let inventoryDiv = document.createElement('div')
-    inventoryDiv.className = 'inventoryDiv'
-    inventoryDiv.textContent = "Beer Name:  " + item.beerName
-    document.querySelector("body > section").append(inventoryDiv)
-
-//<div> containing <button>, <input>, and quantity <p>
-    const quantDiv = document.createElement('div')
-    inventoryDiv.append(quantDiv)
-
-//add <p> for text content to get out of string??
-    let paragraph = document.createElement('p')
-    paragraph.textContent = "Quantity:  " + item.quantity
-    quantDiv.append(paragraph)
-    
-//add input <div>
-    let quantInput = document.createElement('input')
-    quantInput.id = item.id
-    quantInput.class = 'quantityInput'
-    quantInput.placeholder = 'Amnt to Add or Remove'
-    quantInput.type = 'number'
-   
- //create and append the button   
-    let button = document.createElement('button')
-    button.textContent = 'Add/Remove'
-    button.id = item.id
-    quantDiv.append(quantInput)
-    quantDiv.append(button)
-
-//create style <div>
-    let styleDiv = document.createElement('div')
-    styleDiv.textContent = "Style:  " + item.style
-    inventoryDiv.append(styleDiv)
-
-//create Alpha Acid <div>
-    let alcoholPercent = document.createElement('div')
-    alcoholPercent.textContent = "Alcohol %:  " + item.alcohol
-    inventoryDiv.append(alcoholPercent)  
-
-//create deleteBeer button
-    const deleteBeer = document.createElement('button')
-    inventoryDiv.append(deleteBeer)
-    deleteBeer.textContent = 'Delete From Inventory'
-    
-//deletes the beer from our page    
-    deleteButton(deleteBeer, item)
-
-//button event listener with PATCH
-    addRemoveButton(button, item, quantInput, paragraph)  
-}
-
-// handle adding an item to inventory database with 'click' event
-document.querySelector("#addItem > input[type=Submit]").addEventListener('click', e => addToInventory(e))
-function addToInventory(){
-let  newInventoryItem = {
-        beerName: document.querySelector("#beerName").value,
-        quantity: document.querySelector("#quantity").value,
-        style: document.querySelector("#style").value,
-        alcohol: document.querySelector("#alcohol").value
-     }
-     appendItem(newInventoryItem)
-     postItem(newInventoryItem)
-     //clear input fields
-      document.querySelector("#beerName").value =''
-      document.querySelector("#quantity").value =''
-      document.querySelector("#style").value =''
-      document.querySelector("#alcohol").value ='' 
-}
-
-// Add/Remove button event function
-function addRemoveButton(button, item, quantInput, paragraph){
-    button.addEventListener('click', e => {
-        e.preventDefault()
-        paragraph.textContent = "Quantity:  " + (item.quantity -= (quantInput.value *-1))
-    quantInput.value = ''
-    patchItem(item)
-    })
-}
-
-//delete from inventory button
-function deleteButton(deleteBeer, item){
-    deleteBeer.addEventListener('click', e =>{
-        e.preventDefault()
-        deleteBeer.parentElement.remove()
-        deleteItem(item.id)
-    })
+    .then(beer => console.log(beer))
 }
